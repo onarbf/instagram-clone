509e242f92021a9b41a207a39a8fa11fd3bba1b8
Onar <onarbf@gmail.com>
2018-03-12 20:45:46 +0000

    pushing from c9

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import {makeExecutableSchema} from 'graphql-tools';
 
 import typeDefs from './schemas';
 import resolvers from './resolvers';
 
+mongoose.Promise = global.Promise;
+
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 });
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+const IP = process.env.IP || HOST;
+const HOSTPORT = '27017';
 const app = express();
 
 // bodyParser is needed just for POST.
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.get('/graphiql', graphiqlExpress({endpointURL: '/graphql' }));
-app.listen(PORT,()=>{console.log('Running GraphQl Server...');});
+app.get('/', (req,res)=>{
+  res.send('works');
+});
+mongoose.connect('mongodb://'+ IP +':' + HOSTPORT + '/test');
+app.listen(PORT,HOST,()=>{console.log('Running GraphQl Server... on port ' + PORT +' '+ HOST );});
